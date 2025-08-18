af6a17171f58fd4f55d0642818af5b787c1c8a6d
Fbulkaya <senin@emailin.com>
2025-08-18 13:32:26 +0300

    Render test amacıyla eklenen değişiklikler

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ import (
 func main() {
 	utils.InitRedis()
 	router := gin.Default()
+	// sample output for Rendering
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
 	router.POST("/route", handlers.HandleRoute)
 	router.POST("/route_with_current", handlers.HandleRouteWithCurrent)
